057d116dd604105bb8c16e0faf8fdf71f6a40441
Yoshiya-g <y.bskmelo7@gmail.com>
2020-04-26 15:59:10 +0900

    ヘッダーのタブでルーティング実装し、画面も切り替わるように実装。 #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Site from "./components/Site";
 import Footer from "./components/Footer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header></Header>
+				<Switch>
+					<Route label="ホーム" exact path="/" component={Home} />
+          <Route label="紹介サイト一覧" path="/site" component={Site} />
+        </Switch>
+      </BrowserRouter>
       <Footer></Footer>
     </div>
   );
