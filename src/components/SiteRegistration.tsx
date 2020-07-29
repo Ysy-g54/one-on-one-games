f6ad7bacabe358073fb5af357cf2b9bc73406dcb
Yoshiya-g <y.bskmelo7@gmail.com>
2020-07-29 15:28:01 +0900

    #6 データのバインディング、スナックバーの追加をしてcommit

--- src/components/SiteRegistration.tsx
+++ src/components/SiteRegistration.tsx
@@ -1,60 +1,149 @@
 import React from "react";
-import { Button, Box, TextField } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import {
+  Box,
+  Button,
+  Snackbar,
+  IconButton,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
-export default class SiteRegistration<SiteState> extends React.Component {
-  constructor(props: any) {
-    super(props);
-    this.state = { siteNm: "", siteUrl: "", mail: "" };
-    this.changeSiteName = this.changeSiteName.bind(this);
-    this.changeSiteUrl = this.changeSiteUrl.bind(this);
-    this.changeMail = this.changeMail.bind(this);
-  }
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    close: {
+      padding: theme.spacing(0.5),
+    },
+  })
+);
 
-  render() {
-    return (
-      <div>
-        <Box mt={8}>
-          <TextField
-            label="サイト名"
-            variant="outlined"
-            onChange={this.changeSiteName}
-          />
-        </Box>
-        <Box mt={4}>
-          <TextField
-            label="サイトURL"
-            variant="outlined"
-            onChange={this.changeSiteUrl}
-          />
-        </Box>
-        <Box mt={4}>
-          <TextField
-            label="あなたのメールアドレス"
-            variant="outlined"
-            onChange={this.changeMail}
-          />
-        </Box>
-        <Box mt={4} mb={4}>
-          <Button variant="contained" color="primary" onClick={this.registerSite}>
-            送信する
-          </Button>
-        </Box>
-      </div>
-    );
-  }
+export interface SnackbarMessage {
+  message: string;
+  key: number;
+}
 
-  changeSiteName(e: any): void {
-    this.setState({ siteNm: e.target.value });
-  }
+export interface State {
+  open: boolean;
+  snackPack: SnackbarMessage[];
+  messageInfo?: SnackbarMessage;
+  form: {
+    siteNm: string;
+    siteUrl: string;
+    mail: string;
+  };
+}
 
-  changeSiteUrl(e: any): void {
-    this.setState({ siteUrl: e.target.value });
-  }
+export default function SiteRegistration() {
+  const [snackPack, setSnackPack] = React.useState<SnackbarMessage[]>([]);
+  const [open, setOpen] = React.useState(false);
+  const [form, setItem] = React.useState({ siteNm: "", siteUrl: "", mail: "" });
+  const [messageInfo, setMessageInfo] = React.useState<
+    SnackbarMessage | undefined
+  >(undefined);
 
-  changeMail(e: any): void {
-    this.setState({ mail: e.target.value });
-  }
-  registerSite(): void {
-    alert("クリックしました!!");
-  }
+  React.useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      // Set a new snack when we don't have an active one
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      // Close an active snack when a new one is added
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const registerSite = () => {
+    let message: string = "";
+    if (!Object.values(form).some((e) => e === "")) {
+      message = "サイトを登録しました!";
+      setItem({ siteNm: "", siteUrl: "", mail: "" });
+    } else {
+      message = "登録に失敗しました。必須入力の部分を入力してください。";
+    }
+    setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
+  };
+
+  const handleClose = (
+    event: React.SyntheticEvent | MouseEvent,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const updateItem = (e: any) => {
+    setItem({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const classes = useStyles();
+  return (
+    <div>
+      <Box mt={2}>
+        <Typography variant="h6">*必須</Typography>
+      </Box>
+      <Box mt={8}>
+        <TextField
+          label="サイト名*"
+          name="siteNm"
+          variant="outlined"
+          value={form.siteNm}
+          onChange={updateItem}
+        />
+      </Box>
+      <Box mt={4}>
+        <TextField
+          label="サイトURL*"
+          name="siteUrl"
+          variant="outlined"
+          value={form.siteUrl}
+          onChange={updateItem}
+        />
+      </Box>
+      <Box mt={4}>
+        <TextField
+          label="あなたのメールアドレス*"
+          name="mail"
+          variant="outlined"
+          value={form.mail}
+          onChange={updateItem}
+        />
+      </Box>
+      <Box mt={4} mb={4}>
+        <Button variant="contained" color="primary" onClick={registerSite}>
+          登録する
+        </Button>
+      </Box>
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        onExited={handleExited}
+        message={messageInfo ? messageInfo.message : undefined}
+        action={
+          <React.Fragment>
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              className={classes.close}
+              onClick={handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </div>
+  );
 }
