d3624e90312c29a568a0aaea7b26396428c8c6dc
Yoshiya-g <y.bskmelo7@gmail.com>
2020-08-13 18:43:01 +0900

    #31 実装してcommit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import welcomePng from "../public/welcome.png";
+import { Box, Button, Typography } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(() =>
   createStyles({
     imgStyle: {
       width: "80%",
       height: "auto",
+    },
+    button: {
+      height: 60,
     },
   })
 );
@@ -17,7 +20,7 @@ function Home() {
   const classes = useStyles();
   return (
     <div className="Home">
-      <Box mb={10}>
+      <Box m={4}>
         <Typography variant="h2">Teach Each Other Funding Routes</Typography>
       </Box>
       <Typography variant="h5">
@@ -25,6 +28,19 @@ function Home() {
         <br />
         ここから新しい趣味・新しいことを見つけたり、助け合いましょう!
       </Typography>
+      <Box m={4}>
+        <Button
+          variant="contained"
+          disableElevation
+          className={classes.button}
+          color="inherit"
+          size="large"
+          component={Link}
+          to="/site-registration"
+        >
+          サイトを登録する
+        </Button>
+      </Box>
       <img src={welcomePng} className={classes.imgStyle} />
     </div>
   );
